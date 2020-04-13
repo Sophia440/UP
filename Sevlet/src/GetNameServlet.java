e6b70ec160df9807e13ed89b1b7a903c8435c09e
Sophia Klimovich <klimovi4.sophia@gmail.com>
2020-04-13 18:28:49 +0300

    task 5

---
+++ Sevlet/src/GetNameServlet.java
@@ -0,0 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "GetNameServlet")
+public class GetNameServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        if (name.length() > 100) {
+            name = name.substring(0, 99);
+        }
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("<html>");
+        printWriter.println("<h1>Name is " + name + "</h1>");
+        printWriter.println("</html>");
+    }
+}
